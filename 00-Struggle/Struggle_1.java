44540d524a1c7447910b219a1f8bcb9407eeb725
aayushk2107 <aayushkumar21072107@gmail.com>
2026-01-23 20:53:05 +0530

    Update Struggle_1.java

--- 00-Struggle/Struggle_1.java
+++ 00-Struggle/Struggle_1.java
@@ -1,3 +1,3 @@
 /*Leetcode question number 53 Maximum subArray
 22\01\2026
-Got stuck in maximum subarray by using two for loops and tryna run it exactly like running sum question of leetcode and got error why cause i am able to make one invariant jsut that */
+Got stuck in maximum subarray by using two for loops and tryna run it exactly like running sum question of leetcode and got error why cause i am not able to make one invariant just that */
